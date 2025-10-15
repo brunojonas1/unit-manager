793d60be756f5751838f09468925ff6dc3b00ea4
Bruno Moreira <brunojonas1@hotmail.com>
2025-10-14 21:31:23 -0300

    feat: adicionar classe Service para definir a regra de negócio. Criação da funcionalidade User para gerenciar login.

--- src/main/java/com/company/unitmanager/controller/UnitController.java
+++ src/main/java/com/company/unitmanager/controller/UnitController.java
@@ -1,47 +1,26 @@
 package com.company.unitmanager.controller;
 
-import java.util.ArrayList;
+import com.company.unitmanager.model.Unit;
+import com.company.unitmanager.service.UnitService;
+
 import java.util.List;
 
-import com.company.unitmanager.exception.InvalidDataException;
-import com.company.unitmanager.model.Unit;
-
 public class UnitController {
-    private static final List<Unit> units = new ArrayList<>();
-    private static int nextId = 1;
 
-    public static Unit registerUnit(String name, String city){
-        if (name == null || name.trim().isEmpty() || city == null ||city.trim().isEmpty()) {
-            throw new InvalidDataException("Nome e cidade não podem ser vazios");
-        }
-        Unit unit = new Unit(name, city);
-        unit.setId(nextId++);
-        units.add(unit);
-        return unit;
+    public static Unit registerUnit(String name, String address){
+        return UnitService.registerUnit(name, address);
     }
 
     public static List<Unit> loadRegisteredUnits() {
-        return units;
+        return UnitService.loadRegisteredUnits();
     }
 
-    public static Unit updateUnit(int id, String newName, String newCity){
-        if (newName == null || newName.trim().isEmpty() || newCity == null ||newCity.trim().isEmpty()){
-            throw new InvalidDataException("Nome e cidade não podem ser vazios");
-        }
-
-        for (Unit u: units){
-            if (u.getId()==id){
-                u.setName(newName.trim());
-                u.setAddress(newCity.trim());
-                return u;
-            }
-        }
-        throw new InvalidDataException("Unidade com ID " + id + " não encontrada");
+    public static Unit updateUnit(int id, String newName, String newAddress){
+        return UnitService.updateUnit(id, newName, newAddress);
     }
 
     public static void deleteUnit(int id){
-        units.removeIf(u -> u.getId() == id);
+        UnitService.deleteUnit(id);
     }
-
 
 }
